9ff480cd36add01df5ff12b82b00670286242ee8
novakvova <novakvova@gmail.com>
2018-10-31 17:31:32 +0200

    Work Room and LiveData

---
+++ app/src/main/java/com/example/novak/listnoteexample/NoteRepository.java
@@ -0,0 +1,21 @@
+package com.example.novak.listnoteexample;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class NoteRepository {
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteRepository(Application application) {
+        NoteDatabase database = NoteDatabase.getInstance(application);
+        noteDao = database.noteDao();
+        allNotes = noteDao.getAllNotes();
+    }
+
+    //public void insert
+
+}
